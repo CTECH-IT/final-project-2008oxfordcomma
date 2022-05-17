ea1fdf5893247603077ea5a6468c489531bb0328
Dre Harm <2008oxfordcomma@gmail.com>
2022-05-16 21:36:00 -0500

    Moved file and added static middleware function

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+/* 
+ * Express only excepts dynamic HTML file so you must inject a 
+ * static middleware function to use a static HTML file with express 
+*/
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function() {
+    console.log("Listening on: 3000");
+});
+
+/* 
+ * Whenever the site receives a request at the root of the site
+    app.get('/', function(req, res) { 
+    res.end('Testing');
+    });
+*/
+
