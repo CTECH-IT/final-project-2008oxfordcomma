fd8817aa601da0e87536aba9ff87e4da13248803
Dre <2008oxfordcomma@gmail.com>
2022-05-16 20:07:17 -0500

    Added express and a line for testing

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-// Put your JavaScript here
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+    console.log("Listening on: 3000");
+});
+
+/* 
+ * Whenever the site receives a request at the root of the site
+*/
+app.get('/', function(req, res) { 
+   res.end('Testing');
+});
