64e152270b1946accb267d4df96a4bf84f831393
Dre Harm <2008oxfordcomma@gmail.com>
2022-05-17 19:44:46 -0500

    User now makes a post request when on root of site

--- main.js
+++ main.js
@@ -11,10 +11,12 @@ app.listen(3000, function() {
     console.log("Listening on: 3000");
 });
 
-/* 
- * Whenever the site receives a request at the root of the site
-    app.get('/', function(req, res) { 
-    res.end('Testing');
-    });
-*/
+ 
+ // Whenever the site receives a request at the root of the site
+app.post('/', function(req, res) { 
+    res.end('success');      
+});
+
+
+
 
