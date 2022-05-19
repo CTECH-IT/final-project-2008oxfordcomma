dfa83a3e094bbd08b9d78ca0e065c0e868da1a41
Dre Harm <2008oxfordcomma@gmail.com>
2022-05-18 19:31:31 -0500

    Implemented body parser

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 /* 
  * Express only excepts dynamic HTML file so you must inject a 
  * static middleware function to use a static HTML file with express 
 */
 app.use(express.static(__dirname + '/public'));
+
+// the default packet is urlencoded so this must be included
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//  however to send to mailchimp you must use a json package so this is also necessary
+app.use(bodyParser.json())
 
 app.listen(3000, function() {
     console.log("Listening on: 3000");
@@ -14,6 +21,7 @@ app.listen(3000, function() {
  
  // Whenever the site receives a request at the root of the site
 app.post('/', function(req, res) { 
+    console.log(req.body.email);
     res.end('success');      
 });
 
